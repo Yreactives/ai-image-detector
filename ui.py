44fe951fd78aab3dfdc852d1591077b2301a96c0
Ronny <103196718+Yreactives@users.noreply.github.com>
2026-09-14 10:53:12 +0700

    fixed dependency and ui

--- ui.py
+++ ui.py
@@ -62,7 +62,7 @@ uploaded_file = st.file_uploader("Choose an Image...", type=["jpg", "png", "jpeg
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_container_width=True)
+    st.image(image, caption="Uploaded Image", width="stretch")
     st.write("Press Analyze Image below to show results")
 
     if st.button("Analyze Image"):
